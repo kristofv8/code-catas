cd438cce312bb7d239dc29c9da5fc3a705f7fa84
Krzysztof Sieradzki <kristofv8@gmail.com>
2021-01-21 17:31:28 +0100

    Added convert

---
+++ Super duper easy.py
@@ -0,0 +1,13 @@
+def problem(a):
+    if isinstance(a, str):
+        return "Error"
+    x = (a * 50 + 6)
+    return x
+
+
+problem(100)
+
+
+
+
+
---
+++ boolean.py
@@ -0,0 +1,7 @@
+def boolean_to_string(b):
+    boolean_to_string = True
+    boolean_to_string = False
+    if b == True:
+         return "True"
+    if b == False:
+        return "False"
