4d2733fadb26a26f19c85ab980c0db3726fb78fc
Krzysztof Sieradzki <kristofv8@gmail.com>
2021-01-24 16:07:20 +0100

    Second try

---
+++ badalphabet.py
@@ -0,0 +1,57 @@
+
+Alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+            's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+if 'a' in Alphabet:
+    print("Position of alphabet: 1")
+if 'b' in Alphabet:
+    print("Position of alphabet: 2")
+if 'c' in Alphabet:
+    print("Position of alphabet: 3")
+if 'd' in Alphabet:
+    print("Position of alphabet: 4")
+if 'e' in Alphabet:
+    print("Position of alphabet: 5")
+if 'f' in Alphabet:
+    print("Position of alphabet: 6")
+if 'g' in Alphabet:
+    print("Position of alphabet: 7")
+if 'h' in Alphabet:
+    print("Position of alphabet: 8")
+if 'i' in Alphabet:
+    print("Position of alphabet: 9")
+if 'j' in Alphabet:
+    print("Position of alphabet: 10")
+if 'k' in Alphabet:
+    print("Position of alphabet: 11")
+if 'l' in Alphabet:
+    print("Position of alphabet: 12")
+if 'm' in Alphabet:
+    print("Position of alphabet: 13")
+if 'n' in Alphabet:
+    print("Position of alphabet: 14")
+if 'o' in Alphabet:
+    print("Position of alphabet: 15")
+if 'p' in Alphabet:
+    print("Position of alphabet: 16")
+if 'r' in Alphabet:
+    print("Position of alphabet: 17")
+if 's' in Alphabet:
+    print("Position of alphabet: 18")
+if 't' in Alphabet:
+    print("Position of alphabet: 19")
+if 'u' in Alphabet:
+    print("Position of alphabet: 20")
+if 'v' in Alphabet:
+    print("Position of alphabet: 21")
+if 'w' in Alphabet:
+    print("Position of alphabet: 22")
+if 'x' in Alphabet:
+    print("Position of alphabet: 23")
+if 'y' in Alphabet:
+    print("Position of alphabet: 24")
+if 'z' in Alphabet:
+    print("Position of alphabet: 25")
+if 'a' in Alphabet:
+    print("Position of alphabet: 26")
+
---
+++ propher alphabet.py
@@ -0,0 +1,17 @@
+def position(alphabet):
+    letter
+    if letter : 'a'
+        return "Position of alphabet: 1"
+    if "a":
+        return "Position of alphabet: 1"
+position("a")
+
+
+
+
+
+
+
+
+
+#a b c d e f g h i j k l m n o p q r s t u v w x y z
