301aed4e96a7e00e20fccba216b2a6d6897df83b
Krzysztof Sieradzki <kristofv8@gmail.com>
2021-01-24 13:28:42 +0100

    Name changes

---
+++ Convert_ a_ boolean_ to_ a_ string.py
@@ -0,0 +1,16 @@
+def boolean_to_string(b, c):
+    if (b) == True:
+        print("True")
+    elif (b) != True:
+        print("False")
+
+    if (c) == False:
+        print("True")
+    elif (c) != False:
+        print("False")
+
+boolean_to_string(True, False)
+
+
+
+
